f40ba1b2593a3d38456c21ca550928fa96ebe5ed
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2024-02-21 10:25:20 +0000

    ...

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,20 @@
+// Test Includes
 #include "pch.h"
 #include "CppUnitTest.h"
+
+// Project Includes
+#include "accessLevel.hpp"
+#include "database.hpp"
+#include "hashing.hpp"
+#include "patient.hpp"
+#include "user.hpp"
+#include "utils.hpp"
+#include "database.cpp"
+#include "hashing.cpp"
+#include "main.cpp"
+#include "patient.cpp"
+#include "user.cpp"
+#include "utils.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,6 +27,31 @@ namespace tests
 		TEST_METHOD(TestMethod1)
 		{
 			Assert::AreEqual(1, 1);
+		}
+		
+		TEST_METHOD(createPatientAccount)
+		{
+			// TODO: Create Patient Account
+
+			Assert::IsTrue(true);
+		}
+
+		TEST_METHOD(deletePatientAccount)
+		{
+			// TODO: Delete Patient Account
+			Assert::IsTrue(true);
+		}
+
+		TEST_METHOD(createStaffAccount)
+		{
+			// TODO: Create Staff Account
+			Assert::IsTrue(true);
+		}
+
+		TEST_METHOD(deleteStaffAccount)
+		{
+			// TODO: Delete Staff Account
+			Assert::IsTrue(true);
 		}
 	};
 }
